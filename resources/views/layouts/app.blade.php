df95721c4de6de8eaf74e97ad58994009734ee39
Mike van Egmond <mikevanegmond1998@outlook.com>
2020-11-13 21:59:53 +0100

    Initial Laravel 7 setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
+    <title>{{ config('app.name', 'Eenmaal Andermaal') }}</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+
+@include('includes.header')
+
+<main class="py-4">
+    @yield('content')
+</main>
+
+@include('includes.footer')
+
+</body>
+</html>
