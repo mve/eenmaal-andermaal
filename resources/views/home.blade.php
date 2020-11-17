808ed792e17b3726ee71067b5ab72789d7a9bded
Mike van Egmond <mikevanegmond1998@outlook.com>
2020-11-17 21:06:10 +0100

    W.I.P. added initial code for homepage styling.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,116 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="hero-section">
+        <h1 class="title text-white">
+            <i class="fas fa-gavel"></i> Eenmaal Andermaal
+        </h1>
+    </div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+    <div class="container pt-4">
+
+        <h2>Uitgelichte veilingen</h2>
+
+        <div class="row py-4">
+
+            <a href="#" class="col-lg-4 col-md-6 mb-4 no-link">
+                <div class="auction-card hover-effect">
+                    <div class="auction-card-image" style="background-image: url('/images/unsplash-ferrari.jpg');">
+                    </div>
+                    <div class="auction-card-body">
+                        <h4>Ferrari</h4>
+
+                        <div class="flex-centered">
+                            <div class="auction-card-data">€ 100.000.000</div>
+                            <div class="auction-card-data">02:13:10</div>
+                        </div>
+
+                        <div class="flex-centered mt-2">
+                            <div class="btn btn-outline-primary">
+                                Bieden
                             </div>
-                        @endif
+                        </div>
 
-                        {{ __('You are logged in!') }}
                     </div>
                 </div>
+            </a>
+
+            <a href="#" class="col-lg-4 col-md-6 mb-4 no-link">
+                <div class="auction-card hover-effect">
+                    <div class="auction-card-image" style="background-image: url('/images/unsplash-polaroid.jpg');">
+                    </div>
+                    <div class="auction-card-body">
+                        <h4>Ferrari</h4>
+
+                        <div class="flex-centered">
+                            <div class="auction-card-data">€ 1</div>
+                            <div class="auction-card-data">00:04:06</div>
+                        </div>
+
+                        <div class="flex-centered mt-2">
+                            <div class="btn btn-outline-primary">
+                                Bieden
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" class="col-lg-4 col-md-6 mb-4 no-link">
+                <div class="auction-card hover-effect">
+                    <div class="auction-card-image" style="background-image: url('/images/unsplash-ferrari.jpg');">
+                    </div>
+                    <div class="auction-card-body">
+                        <h4>Ferrari</h4>
+
+                        <div class="flex-centered">
+                            <div class="auction-card-data">€ 100.000.000</div>
+                            <div class="auction-card-data">02:13:10</div>
+                        </div>
+
+                        <div class="flex-centered mt-2">
+                            <div class="btn btn-outline-primary">
+                                Bieden
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" class="col-lg-4 col-md-6 mb-4 no-link">
+                <div class="auction-card hover-effect">
+                    <div class="auction-card-image" style="background-image: url('/images/unsplash-ferrari.jpg');">
+                    </div>
+                    <div class="auction-card-body">
+                        <h4>Ferrari</h4>
+
+                        <div class="flex-centered">
+                            <div class="auction-card-data">€ 100.000.000</div>
+                            <div class="auction-card-data">02:13:10</div>
+                        </div>
+
+                        <div class="flex-centered mt-2">
+                            <div class="btn btn-outline-primary">
+                                Bieden
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+        <h2>Categoriën</h2>
+
+        <div class="row">
+            <div class="col-md-3">
+
             </div>
         </div>
+
     </div>
 
 @endsection
