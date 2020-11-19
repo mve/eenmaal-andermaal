f26dd08ccaea54e3d380ca52b5176db27274c291
Mike van Egmond <mikevanegmond1998@outlook.com>
2020-11-19 17:06:50 +0100

    Fixed incorrect G

--- db.sql
+++ db.sql
@@ -164,7 +164,7 @@ CREATE TABLE eenmaalandermaal.dbo.auction_categories (
 	CONSTRAINT auction_categories_PK PRIMARY KEY (id),
 	CONSTRAINT auction_categories_FK FOREIGN KEY (auction_id) REFERENCES eenmaalandermaal.dbo.auction(id),
 	CONSTRAINT auction_categories_FK_1 FOREIGN KEY (category_id) REFERENCES eenmaalandermaal.dbo.categories(id)
-) G
+)
 
 
 -- eenmaalandermaal.dbo.auction_hits definition
