811ed8bcee8255028203ebb9fbd66d3d36ffedda
GoldenHeartbell <geurian2000@gmail.com>
2020-11-20 10:52:45 +0100

    Inlog pagina doorgeleiden wanneer ingelogd

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
+        if ($request->session()->has('user')) {
             return redirect(RouteServiceProvider::HOME);
         }
 
