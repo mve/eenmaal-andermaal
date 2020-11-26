653616219e5f4d4a3a9f0833d9b0a1bb14b38dc6
Guusboyy <38752370+Guusboyy@users.noreply.github.com>
2020-11-26 15:22:19 +0100

    vragen verzonnen

--- public/js/app.js
+++ public/js/app.js
@@ -27022,7 +27022,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-
 __webpack_require__(/*! C:\Users\guusn\OneDrive\Desktop\EENMAAL ANDERMAAL\eenmaal-andermaal\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Users\guusn\OneDrive\Desktop\EENMAAL ANDERMAAL\eenmaal-andermaal\resources\sass\app.scss */"./resources/sass/app.scss");
 
--- resources/views/faq/faq.blade.php
+++ resources/views/faq/faq.blade.php
@@ -9,40 +9,42 @@
         <div class="accordion" id="vragenlijst">
             <div class="accordion-item my-3">
                 <h2 class="accordion-header" id="vraag1">
-                    <button class="accordion-button" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
-                        Hoe kan ik meebieden?
+                    <button class="accordion-button" type="button" data-toggle="collapse" data-target="#collapse1"
+                            aria-expanded="true" aria-controls="collapse1">
+                        Hoe bied ik op een veiling?
                     </button>
                 </h2>
                 <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="vraag1"
                      data-parent="#vragenlijst">
                     <div class="accordion-body">
-                        door een account aan te maken op de volgende link url
+                        <ul>
+                            <li>Zorg dat je bent ingelogd met een account.</li>
+                            <li>klik op een veiling waarop je wil je bieden. Vervolgens heb je hier de mogelijkheid om
+                                een veiling te bekijken en daarop te bieden.
+                            </li>
+                            <li>Is de veiling verlopen en heb jij het hoogste bod. dan krijg je een bericht met de
+                                details van de veiling.
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
             <div class="accordion-item my-3">
                 <h2 class="accordion-header" id="vraag2">
-                    <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
-                        Hoe maak ik een account aan
+                    <button class="accordion-button collapsed" type="button" data-toggle="collapse"
+                            data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                        Hoe maak ik een account aan?
                     </button>
                 </h2>
                 <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="vraag2"
                      data-parent="#vragenlijst">
                     <div class="accordion-body">
-                        stappen plan
-                    </div>
-                </div>
-            </div>
-            <div class="accordion-item my-3">
-                <h2 class="accordion-header" id="vraag3">
-                    <button class="accordion-button collapsed" type="button" data-toggle="collapse"  data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
-                        vraag 3
-                    </button>
-                </h2>
-                <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="vraag3"
-                     data-parent="#vragenlijst">
-                    <div class="accordion-body">
-                        test
+                        <ul>
+                            <li>Ga naar de pagina registreren. Volg hier de stappen die te nemen zijn om te registreren:</li>
+                            <li>Stap 1: Voer je gebruikersnaam en je email adres in. Vervolgens ontvang je een verificatie code op je mail adres.</li>
+                            <li>Stap 2: Voer je gekregen verificatie code in die je gekregen hebt op de mail.</li>
+                            <li>Stap 3: Wanneer de verificatie code klopt kun je de rest van je account gegevens invoeren.</li>
+                        </ul>
                     </div>
                 </div>
             </div>
