0a4372aa884858067320cd9e981a0e7d6d9f61e1
Roel Stevens <rshunter99@gmail.com>
2020-12-16 17:39:26 +0100

    view individual user page

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -40,13 +40,58 @@ class UserController extends Controller
     public function view(Request $request)
     {
         $user = User::oneWhere('id', $request->id);
-        $auctions = Auction::resultArrayToClassArray(DB::select('SELECT title, end_datetime FROM auctions WHERE user_id =:user_id ORDER BY end_datetime DESC', [
-            'user_id' => $user->id
-        ]));
+
+        // -- SQL query
+        // SELECT * FROM dbo.auctions a
+        // OUTER APPLY (
+        //     SELECT TOP 1 * 
+        //     FROM dbo.bids b
+        //     WHERE a.id = b.auction_id
+        //     ORDER BY b.bid_datetime DESC
+        // ) as b
+        // -- optional: WHERE a.user_id = {{userid}}
+
+        $sql = 'SELECT a.title, a.end_datetime, b.amount 
+        FROM auctions a
+        OUTER APPLY (
+            SELECT TOP 1 b.amount 
+            FROM bids b 
+            WHERE a.id = b.auction_id 
+            ORDER BY b.bid_datetime DESC
+        ) as b 
+        WHERE a.user_id =:user_id AND a.end_datetime /xd/ GETDATE()
+        ORDER BY a.end_datetime DESC';
+
+        $auctions = Auction::resultArrayToClassArray(DB::select(str_replace("/xd/", ">=", $sql), ['user_id' => $user->id]));
+        $pastAuctions = Auction::resultArrayToClassArray(DB::select(str_replace("/xd/", "<", $sql), ['user_id' => $user->id]));
+        $bids = Auction::resultArrayToClassArray(DB::select('
+            DECLARE @userId int
+            SET @userId =:user_id
+            
+            SELECT (
+                SELECT COUNT(id)
+                FROM dbo.bids
+                WHERE user_id = @userId
+            ) AS placed,
+            (
+                SELECT ISNULL(SUM(amount), 0)
+                FROM dbo.bids
+                WHERE user_id = @userId
+            ) AS amount_placed,
+            (
+                SELECT COUNT(id)
+                FROM dbo.bids WHERE auction_id IN (SELECT id FROM dbo.auctions WHERE user_id = @userId)
+            ) AS received,
+            (
+                SELECT ISNULL(SUM(amount), 0)
+                FROM dbo.bids WHERE auction_id IN (SELECT id FROM dbo.auctions WHERE user_id = @userId)
+            ) AS amount_received', ['user_id' => $user->id]));
 
         $data = [
             'user' => $user,
-            'auctions' => $auctions
+            'auctions' => $auctions,
+            'pastAuctions' => $pastAuctions,
+            'bids' => $bids[0]
         ];
         return view('admin.users.view')->with($data);
     }    
--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -2,61 +2,158 @@
 
 @section('content')
 
-    <div class="container-fluid admin">
+<div class="container-fluid admin">
 
-        <h1 class="text-center pt-3"> "pieter jan"</h1>
-
-        <div class="content">
-
-            <h1>{{ $user->username }}</h1>
-            <div class="row">
-                <div class="col-lg-4 text-center">
-                    <h2 class="text-center">Veilingen</h2>
-                    <div class="card">
-                        <div class="card-body">
-                            <p>Actief</p>
-                            <h1>5</h1>
-                            <p>Totaal</p>
-                            <h1>13</h1>
+    <h1 class="text-center pt-3">{{ $user->username }}</h1>
+    <div class="content">
+        <div class="row">
+            <div class="col-lg-3 text-center">
+                <h2>Veilingen</h2>
+                <div class="row rounded-top border-top pt-2 mx-1">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <p>Actief</p>
+                                        <h1 class="display-2">{{count($auctions)}}</h1>
+                                    </div>
+                                    <div class="col">
+                                        <p>Totaal</p>
+                                        <h1 class="display-2">{{count($auctions + $pastAuctions)}}</h1>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 text-center">
-                    <h2>Biedingen</h2>
-                    <div class="card">
-                        <div class="card-body">
-                            <p>Actief</p>
-                            <h1>5</h1>
-                            <p>Totaal</p>
-                            <h1>13</h1>
+            </div>
+            <div class="col-lg-6 text-center">
+                <h2>Biedingen</h2>
+                <div class="row rounded-top border-top pt-2 mx-1">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <p>Ontvangen</p>
+                                    <div class="col">
+                                        <h1 class="display-2">€{{number_format($bids->amount_received, 2)}}</h1>
+                                    </div>
+                                    <div class="col">
+                                        <h1 class="display-2">{{$bids->received}}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <p>Geplaatst</p>
+                                    <div class="col">
+                                        <h1 class="display-2">€{{number_format($bids->amount_placed, 2)}}</h1>
+                                    </div>
+                                    <div class="col">
+                                        <h1 class="display-2">{{$bids->placed}}</h1>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 text-center">
-                    <h2>Informatie</h2>
-                    <div class="card text-left">
-                        <div class="card-body">
-                            <table class="table">
-                                <tbody>
-                                <tr>
-                                    <td>Mark</td>
-                                    <td>Mark</td>
-                                </tr>
-                                <tr>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                </tr>
-                                <tr>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                </tr>
-                                </tbody>
-                            </table>
+            </div>
+            <div class="col-lg-3 text-center">
+                <h2>Informatie</h2>
+                <div class="row rounded-top border-top pt-2 mx-1">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <td class="text-left">Naam</td>
+                                            @if($user->first_name === $user->last_name)
+                                                <td class="text-right">{{$user->last_name}}</td>
+                                            @else
+                                                <td class="text-right">{{$user->first_name}} {{$user->last_name}}</td>
+                                            @endif
+                                            
+                                        </tr>
+                                        <tr>
+                                            <td class="text-left">E-mail</td>
+                                            <td class="text-right">{{ $user->email }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-left">Geboortedatum</td>
+                                            <td class="text-right">{{ $user->birth_date }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-left">Woonplaats</td>
+                                            <td class="text-right">{{ $user->city }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-left">Land</td>
+                                            <td class="text-right">{{ $user->country_code }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-left">Registratiedatum</td>
+                                            <td class="text-right">{{date('d-m-Y', strtotime($user->created_at))}}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <button class="btn btn-danger text-light btn-lg">Blokkeren</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        @if(count($auctions) > 0)
+            <h3>Actieve veilingen</h3>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Titel</th>
+                        <th scope="col">Huidig bod</th>
+                        <th scope="col">Einddatum</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        @foreach($auctions as $auction)                
+                            <tr>
+                                <td>{{$auction->title}}</td>
+                                <td>{{$auction->amount}}</td>
+                                <td>{{$auction->end_datetime}}</td>
+                            </tr>
+                        @endforeach
+                </tbody>
+            </table>
+        @endif
+
+        @if(count($pastAuctions) > 0)
+        <h3>Historie</h3>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Titel</th>
+                        <th scope="col">Huidig bod</th>
+                        <th scope="col">Einddatum</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        @foreach($auctions as $auction)                
+                            <tr>
+                                <td>{{$auction->title}}</td>
+                                <td>{{$auction->amount}}</td>
+                                <td>{{$auction->end_datetime}}</td>
+                            </tr>
+                        @endforeach
+                </tbody>
+            </table>
+        @endif
     </div>
+</div>
 
 @endsection
