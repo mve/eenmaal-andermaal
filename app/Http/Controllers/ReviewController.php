9c44066959ea835179c5c7f3b848a3cad1c522af
Mike van Egmond <mikevanegmond1998@outlook.com>
2020-12-16 17:14:28 +0100

    Added reviews page.

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Auction;
+use App\DB;
 use App\Review;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,20 @@ class ReviewController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('check.user.reviewed.auction');
+        $this->middleware('check.user.reviewed.auction')->except(['index']);
+    }
+
+    public function index()
+    {
+        $user_id = 2065;
+
+        $data = DB::select("SELECT r.id AS review_id, r.auction_id, r.user_id, r.review_datetime, r.rating, r.comment, u.username, u.email, u.first_name, u.last_name, a.title AS auction_title, a.description AS auction_description
+            FROM reviews r
+            LEFT JOIN users u ON r.user_id = u.id
+            LEFT JOIN auctions a ON r.auction_id = a.id
+            WHERE r.user_id = " . $user_id);
+
+        return view("reviews.index")->with('data', $data);
     }
 
     public function create(Request $request)
@@ -31,18 +45,18 @@ class ReviewController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, array(
-            'rating' => ['required', 'numeric','min:2', 'max:5'],
-            'comment' => ['nullable','string', 'max:255'],
+            'rating' => ['required', 'numeric', 'min:2', 'max:5'],
+            'comment' => ['nullable', 'string', 'max:255'],
         ));
 
         $review = new Review();
         $review->auction_id = $request->get("auction")->id;
         $review->user_id = $request->session()->get("user")->id;
         $review->rating = $request->rating;
-        if($request->comment!==null)
+        if ($request->comment !== null)
             $review->comment = $request->comment;
         $review->save();
-        $request->session()->flash('success', 'Beoordeling voor <b>'.$request->get("auction")->title.'</b> is geplaatst!');
+        $request->session()->flash('success', 'Beoordeling voor <b>' . $request->get("auction")->title . '</b> is geplaatst!');
         return redirect()->route('veilingen.gewonnen');
     }
 }
