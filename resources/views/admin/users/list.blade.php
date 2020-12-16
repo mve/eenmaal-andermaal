d5af220d9527bcaba53fe83c27034ea5f66833bd
Guusboyy <38752370+Guusboyy@users.noreply.github.com>
2020-12-16 14:39:11 +0100

    kleine layout fix

--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
 
-    <div class="container-fluid admin"
-         style="padding-left: 290px; padding-top: 40px; background-color: #353535; height: 100vh;">
-        <h1>List all users</h1>
-        
+    <div class="container-fluid admin">
+        <h1 class="text-center pt-3"> Eenmaal andermaal</h1>
+
+        <div class="content">
+            <h1>List all users</h1>
+        </div>
     </div>
 
 @endsection
--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -2,39 +2,42 @@
 
 @section('content')
 
-    <div class="container-fluid admin"
-         style="padding-left: 290px; padding-top: 40px; background-color: #353535; height: 100vh;">
+    <div class="container-fluid admin">
 
-        <h1>{{ $user->username }}</h1>
-        <div class="row">
-            <div class="col-lg-4 text-center">
-                <h2 class="text-center">Veilingen</h2>
-                <div class="card">
-                    <div class="card-body">
-                        <p>Actief</p>
-                        <h1>5</h1>
-                        <p>Totaal</p>
-                        <h1>13</h1>
+        <h1 class="text-center pt-3"> "pieter jan"</h1>
+
+        <div class="content">
+
+            <h1>{{ $user->username }}</h1>
+            <div class="row">
+                <div class="col-lg-4 text-center">
+                    <h2 class="text-center">Veilingen</h2>
+                    <div class="card">
+                        <div class="card-body">
+                            <p>Actief</p>
+                            <h1>5</h1>
+                            <p>Totaal</p>
+                            <h1>13</h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 text-center">
-                <h2>Biedingen</h2>
-                <div class="card">
-                    <div class="card-body">
-                        <p>Actief</p>
-                        <h1>5</h1>
-                        <p>Totaal</p>
-                        <h1>13</h1>
+                <div class="col-lg-4 text-center">
+                    <h2>Biedingen</h2>
+                    <div class="card">
+                        <div class="card-body">
+                            <p>Actief</p>
+                            <h1>5</h1>
+                            <p>Totaal</p>
+                            <h1>13</h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 text-center">
-                <h2>Informatie</h2>
-                <div class="card text-left">
-                    <div class="card-body">
-                        <table class="table">
-                            <tbody>
+                <div class="col-lg-4 text-center">
+                    <h2>Informatie</h2>
+                    <div class="card text-left">
+                        <div class="card-body">
+                            <table class="table">
+                                <tbody>
                                 <tr>
                                     <td>Mark</td>
                                     <td>Mark</td>
@@ -47,13 +50,13 @@
                                     <td>Larry</td>
                                     <td>the Bird</td>
                                 </tr>
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 
 @endsection
