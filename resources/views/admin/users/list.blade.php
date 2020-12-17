06d13b891baa63b12b2930d66ddda2a18fb6445f
Guusboyy <38752370+Guusboyy@users.noreply.github.com>
2020-12-17 14:37:21 +0100

    ophalen gegevens gebruikers en in tabel stoppen

--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -1,71 +1,53 @@
 @extends('layouts.admin-app')
 
 @section('content')
-
-    <div class="container-fluid admin">
+<div class="admin">
+    <div class="container-fluid ">
         <h1 class="text-center pt-3">Gebruikers</h1>
 
         <div class="content">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead class="thead">
-                    <tr>
-                        <th scope="col">Gebruikersnaam</th>
-                        <th scope="col">E-mail</th>
-                        <th scope="col">Naam</th>
-                        <th scope="col" class="endTd">Verkoper</th>
-                        <th>Actief</th>
-                        <th>Totaal</th>
 
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    @foreach($users as $user)
-
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="thead">
                         <tr>
-
-                            <td>{{$user->username}}</td>
-                            <td>{{$user->email}}</td>
-                            <td>{{$user->first_name}} {{$user->last_name}}</td>
-                            <td class="endTd">{{$user->is_seller}}</td>
-
+                            <th scope="col">Gebruikersnaam</th>
+                            <th scope="col">E-mail</th>
+                            <th scope="col">Naam</th>
+                            <th scope="col">Land</th>
+                            <th scope="col">Verkoper</th>
+                            <th scope="col" class="endTd">Geblokkeerd</th>
+                            <th>Actief</th>
+                            <th>Totaal</th>
                         </tr>
-                    @endforeach
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td class="endTd">@mdo</td>
-                        <td>8</td>
-                        <td>29</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td class="endTd">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" disabled>
-                            </div>
-                        </td>
-                        <td>8</td>
-                        <td>29</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td class="endTd">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" disabled checked>
-                            </div>
-                        </td>
-                        <td>8</td>
-                        <td>29</td>
-                    </tr>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach($users as $user)
+                            <tr onclick="window.location.assign('{{route('users.view', $user->id)}}')">
+                                <td>{{$user->username}}</td>
+                                <td>{{$user->email}}</td>
+                                <td>@if ($user->first_name === $user->last_name)
+                                        {{$user->first_name}}
+                                    @else
+                                        {{$user->first_name}} {{$user->last_name}}
+                                    @endif</td>
+                                <td>{{$user->country_code}}</td>
+                                <td>
+                                    @if($user->is_seller == 1)
+                                        <i class="fas fa-check-circle"></i>
+                                    @endif
+                                </td>
+                                <td class="endTd">
+                                    @if($user->is_blocked == 1)
+                                        <i class="fas fa-ban"></i>
+                                    @endif</td>
+                                <td>{{$user->Active}}</td>
+                                <td>{{$user->AllCnt}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
