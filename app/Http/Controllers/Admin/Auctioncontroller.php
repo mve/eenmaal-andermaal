5a10225b1895b91519157ffb65bd4ea389781b3e
Guusboyy <38752370+Guusboyy@users.noreply.github.com>
2021-01-04 15:31:37 +0100

    auctions ophalen en in tabel plaatsen

--- app/Http/Controllers/Admin/Auctioncontroller.php
+++ app/Http/Controllers/Admin/Auctioncontroller.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Auction;
+use App\AuctionHit;
+use App\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class Auctioncontroller extends Controller
 {
@@ -20,6 +24,20 @@ class Auctioncontroller extends Controller
     public function list()
     {
 
-        return view('admin.auctions.list');
+        $auctions = DB::select("
+            SELECT TOP 10 auctions.* , users.first_name , users.last_name
+            FROM auctions LEFT JOIN users
+            ON users.id = auctions.user_id
+            ORDER BY auctions.id
+            ");
+
+
+        $auctions = Auction::resultArrayToClassArray($auctions);
+
+        $data = [
+            "auctions" => $auctions,
+        ];
+       // dd($data);
+        return view('admin.auctions.list')->with($data);
     }
 }
--- resources/views/admin/auctions/list.blade.php
+++ resources/views/admin/auctions/list.blade.php
@@ -11,18 +11,35 @@
                     <table class="table">
                         <thead class="thead">
                         <tr>
-                            <th scope="col">Gebruikersnaam</th>
-                            <th scope="col">E-mail</th>
-                            <th scope="col">Naam</th>
-                            <th scope="col">Land</th>
-                            <th scope="col">Verkoper</th>
-                            <th scope="col" class="endTd">Geblokkeerd</th>
-                            <th>Actief</th>
-                            <th>Totaal</th>
+                            <th>Titel</th>
+                            <th>Plaats</th>
+                            <th>Land</th>
+                            <th>Verkoper</th>
+                            <th>Startprijs</th>
+                            <th>Aantal dagen</th>
+                            <th class="endTd">Eind datum</th>
+                            <th>Geblokkeerd</th>
                         </tr>
                         </thead>
                         <tbody>
-
+                        @foreach($auctions as $auction)
+                            <tr onclick="window.location.assign('{{route('auctions.view', $auction->id)}}')">
+                                <td>{{$auction->title}}</td>
+                                <td>{{$auction->city}}</td>
+                                <td>{{$auction->country_code}}</td>
+                                <td>@if ($auction->first_name === $auction->last_name)
+                                        {{$auction->first_name}}
+                                    @else
+                                        {{$auction->first_name}} {{$auction->last_name}}
+                                    @endif</td>
+                                <td>€ {{$auction->start_price}}</td>
+                                <td>{{$auction->duration}}</td>
+                                <td class="endTd">{{date('d-m-Y', strtotime($auction->end_datetime))}}</td>
+                                <td>@if($auction->is_blocked == 1)
+                                        <i class="fas fa-ban"></i>
+                                    @endif</td>
+                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
