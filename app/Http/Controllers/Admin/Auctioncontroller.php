df9fad8ef1c81c6d58a5d40fcf42373e9a8d4199
Roel Stevens <rshunter99@gmail.com>
2021-01-05 12:29:52 +0100

    View auction admin page design

--- app/Http/Controllers/Admin/Auctioncontroller.php
+++ app/Http/Controllers/Admin/Auctioncontroller.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Bid;
 use App\DB;
 use App\Auction;
+use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -27,12 +29,41 @@ class Auctioncontroller extends Controller
     public function view(Request $request)
     {
         $auction = Auction::oneWhere('id', $request->id);
+        // $auction = Auction::resultArrayToClassArray(DB::selectOne('
+        //     select a.id, a.title, a.description, a.start_price, a.duration, a.end_datetime, a.user_id, a.created_at, u.first_name, u.last_name
+        //     FROM auctions AS a
+        //     INNER JOIN users AS u 
+        //     ON a.user_id = u.id
+        //     AND a.id =:auction_id
+        // ', ['auction_id' => $request->id]));
+
+        if ($auction === false) {
+            return abort(404);
+        }
+
+        $user = User::resultArrayToClassArray(DB::select(
+            'SELECT first_name, last_name 
+            FROM users WHERE id =:user_id',
+            ['user_id' => $auction->user_id]
+        ));
+
+        $bids = Bid::resultArrayToClassArray(DB::select(
+            'SELECT max(amount) as amount, count(amount) as count 
+            FROM bids 
+            WHERE auction_id =:auction_id 
+            GROUP BY auction_id',
+            ['auction_id' => $auction->id]
+        ));
+
         $auctionImages = $auction->getImages();
 
         $data = [
             'auction' => $auction,
-            'auctionImages' => $auctionImages
+            'auctionImages' => $auctionImages,
+            'user' => $user[0],
+            'bids' => $bids ? $bids[0] : null
         ];
+        
         return view('admin.auctions.view')->with($data);
     }
 
--- resources/views/admin/auctions/view.blade.php
+++ resources/views/admin/auctions/view.blade.php
@@ -5,7 +5,7 @@
 <div class="container-fluid admin">
     <h2>{{$auction->title}}</h2>
     <div class="row">
-        <div class="col-lg-7">
+        <div class="col-lg-8">
             <!-- CAROUSEL SLIDER -->
             <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
                 <ol class="carousel-indicators">
@@ -31,119 +31,140 @@
             @endif
             <!-- END OF CAROUSEL SLIDER -->
         </div>
-        <div class="col-lg-5 text-center">
-            <h2>Informatie</h2>
-            <div class="row rounded-top border-top pt-2 mx-1">
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body">
-                            <table class="table">
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <div class="row">
-                                                <div class="col text-left">Naam</div>
-                                                <div class="col text-right">
-
-                                                </div>
+    </div>
+    <div class="col-lg text-center">
+        <h2>Informatie</h2>
+        <div class="row rounded-top border-top pt-2 mx-1">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div class="row">
+                                            <div class="col text-left">Verkoper</div>
+                                            <div class="col text-right">
+                                                @if ($user->first_name === $user->last_name)
+                                                {{$user->first_name}}
+                                                @else
+                                                {{$user->first_name}} {{$user->last_name}}
+                                                @endif
                                             </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <div class="row">
-                                                <div class="col text-left">E-mail</div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <div class="row">
-                                                <div class="col text-left">Geboortedatum</div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <div class="row">
-                                                <div class="col text-left">Woonplaats</div>
-
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <div class="row">
-                                                <div class="col text-left">Land</div>
-
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <div class="row">
-                                                <div class="col text-left">Registratiedatum</div>
-
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <form method="post" action="">
-                                @csrf
-
-                                <button class="btn btn-success btn-lg" type="submit" name="unblock" onclick="return confirm('Weet u zeker dat u deze gebruiker wilt deblokkeren?')">Deblokkeren</button>
-
-                                <button class="btn btn-danger text-light btn-lg" type="submit" name="block" onclick="return confirm('Weet u zeker dat u deze gebruiker wilt blokkeren?')">Blokkeren</button>
-
-                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="row mt-4">
+                                            <div class="col text-left">Startdatum</div>
+                                            <div class="col text-right">{{date('d-m-Y', strtotime($auction->created_at))}}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="row">
+                                            <div class="col text-left">Looptijd</div>
+                                            <div class="col text-right">{{$auction->duration}} dagen</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="row">
+                                            <div class="col text-left">Einddatum</div>
+                                            <div class="col text-right">{{date('d-m-Y', strtotime($auction->end_datetime))}}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="row mt-4">
+                                            <div class="col text-left">Startprijs</div>
+                                            <div class="col text-right">€{{number_format($auction->start_price)}}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @if ($bids)
+                                <tr>
+                                    <td>
+                                        <div class="row">
+                                            <div class="col text-left">Aantal biedingen</div>
+                                            <div class="col text-right">{{$bids->count}}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="row">
+                                            <div class="col text-left">Huidig bedrag</div>
+                                            <div class="col text-right">€{{$bids->amount}}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                        <div class="row">
+                            <div class="col text-right">
+                                <button class="btn btn-primary text-light btn-lg" name="block" onclick="window.location.assign('{{route('admin.users.view', $auction->user_id)}}')">Zie verkoper</button>
+                            </div>
+                            <div class="col text-left">
+                                <form method="post" action="">
+                                    @csrf
+                                    @if ($auction->is_blocked)
+                                    <button class="btn btn-success btn-lg" type="submit" name="unblock" onclick="return confirm('Weet u zeker dat u deze veiling wilt deblokkeren?')">Deblokkeren</button>
+                                    @else
+                                    <button class="btn btn-danger text-light btn-lg" type="submit" name="block" onclick="return confirm('Weet u zeker dat u deze veiling wilt blokkeren?')">Blokkeren</button>
+                                    @endif
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="rol">
-            <div class="my-4">
-                <h4>Omschrijving</h4>
-                <hr>
-                <p>{!! nl2br($auction->description) !!}</p>
-                <h4>Gegevens</h4>
-                <hr>
-                <p>
-                    Land: {{$auction->getCountry()}}<br />
-                    Plaats: {{$auction->city}}
-                    @foreach($auction->getSeller()->getPhoneNumbers() as $phoneNumber)
-                    <br />Telefoonnummer: {{$phoneNumber["phone_number"]}}<br />
-                    @endforeach
-                </p>
-                <h4>Betaling</h4>
-                <hr>
-                <p>
-                    @if($auction->payment_instruction!==null && !empty($auction->payment_instruction))
-                    Betalingsinstructies:<br />
-                    {{$auction->payment_instruction}}<br /><br />
-                    @endif
-                    De verkoper accepteert de volgende betalingsmethoden:<br />
-                <ul>
-                    @foreach($auction->getPaymentMethods() as $paymentMethod)
-                    <li>{{$paymentMethod["method"]}}</li>
-                    @endforeach
-                </ul>
-                </p>
-                <h4>Verzending</h4>
-                <hr>
-                <p>
-                    De verkoper accepteert de volgende verzendmethoden:<br />
-                <ul>
-                    @foreach($auction->getShippingMethods() as $shippingMethod)
-                    <li>{{$shippingMethod["method"]}}: &euro;{{$shippingMethod["price"]}}</li>
-                    @endforeach
-                </ul>
-                </p>
-            </div>
+</div>
+<div class="row">
+    <div class="rol">
+        <div class="my-4">
+            <h4>Omschrijving</h4>
+            <p>{!! nl2br($auction->description) !!}</p>
+            <h4>Gegevens</h4>
+            <p>
+                Land: {{$auction->getCountry()}}<br />
+                Plaats: {{$auction->city}}
+                @foreach($auction->getSeller()->getPhoneNumbers() as $phoneNumber)
+                <br />Telefoonnummer: {{$phoneNumber["phone_number"]}}<br />
+                @endforeach
+            </p>
+            <h4>Betaling</h4>
+            <p>
+                @if($auction->payment_instruction!==null && !empty($auction->payment_instruction))
+                Betalingsinstructies:<br />
+                {{$auction->payment_instruction}}<br /><br />
+                @endif
+                De verkoper accepteert de volgende betalingsmethoden:<br />
+            <ul>
+                @foreach($auction->getPaymentMethods() as $paymentMethod)
+                <li>{{$paymentMethod["method"]}}</li>
+                @endforeach
+            </ul>
+            </p>
+            <h4>Verzending</h4>
+            <p>
+                De verkoper accepteert de volgende verzendmethoden:<br />
+            <ul>
+                @foreach($auction->getShippingMethods() as $shippingMethod)
+                <li>{{$shippingMethod["method"]}}: &euro;{{$shippingMethod["price"]}}</li>
+                @endforeach
+            </ul>
+            </p>
         </div>
     </div>
+</div>
 </div>
 
 @endsection
